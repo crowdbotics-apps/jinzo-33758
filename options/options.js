c160c0763e7d82c4f9c616b58fb9380489fdbb8c
Crowdbotics <team@crowdbotics.com>
2022-02-21 14:57:00 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -21,10 +21,10 @@ export const modulesOptions = {
     SignInButtonText: "Login",
     SignUpButtonText: "Sign Up"
   },
+
   "@modules/login": {
     LOGO_URL: "",
-    BACKGROUND_URL:
-      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/halfbg.png",
+    BACKGROUND_URL: "",
     HOME_SCREEN_NAME: "Home Screen",
     validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
     SignInNavText: "Sign In",
